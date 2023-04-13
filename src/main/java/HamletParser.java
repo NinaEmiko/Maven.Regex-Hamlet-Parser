b1e62dcd787ae509478f072d5c2b853e03572073
Nina <nina.emiko.mcnair@gmail.com>
2023-04-12 20:35:00 -0400

    All tests passing

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,16 +1,65 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
  */
 public class HamletParser {
 
-    private String hamletData;
+    private static String hamletData;
 
     public HamletParser(){
         this.hamletData = loadFile();
+    }
+
+    public static void changeHamletToLeon() {
+        Pattern pattern = Pattern.compile("Hamlet");
+        Matcher matcher = pattern.matcher(hamletData);
+        StringBuffer sb = new StringBuffer();
+        String replacementText = "Leon";
+        while(matcher.find()){
+            matcher.appendReplacement(sb, replacementText);
+        }
+        matcher.appendTail(sb);
+        hamletData = sb.toString();
+
+        pattern = Pattern.compile("HAMLET");
+        matcher = pattern.matcher(hamletData);
+        sb = new StringBuffer();
+        String replacementText2 = "LEON";
+        while(matcher.find()){
+            matcher.appendReplacement(sb, replacementText2);
+        }
+        matcher.appendTail(sb);
+
+        hamletData = sb.toString();
+//        hamletData = sb.toString();
+    }
+
+    public static void changeHoratioToTariq() {
+        Pattern pattern = Pattern.compile("Horatio");
+        Matcher matcher = pattern.matcher(hamletData);
+        StringBuffer sb = new StringBuffer();
+        String replacementText = "Tariq";
+        while(matcher.find()){
+            matcher.appendReplacement(sb, replacementText);
+        }
+        matcher.appendTail(sb);
+        hamletData = sb.toString();
+
+        pattern = Pattern.compile("HORATIO");
+        matcher = pattern.matcher(hamletData);
+        sb = new StringBuffer();
+        String replacementText2 = "TARIQ";
+        while(matcher.find()){
+            matcher.appendReplacement(sb, replacementText2);
+        }
+        matcher.appendTail(sb);
+
+        hamletData = sb.toString();
     }
 
     private String loadFile(){
@@ -36,4 +85,23 @@ public class HamletParser {
         return hamletData;
     }
 
+    public int findHoratio() {
+        Pattern pattern = Pattern.compile("Horatio", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(hamletData);
+        int count = 0;
+        while (matcher.find()) {
+            count++;
+        }
+        return count;
+    }
+
+    public int findHamlet() {
+        Pattern pattern = Pattern.compile("Hamlet", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(hamletData);
+        int count = 0;
+        while (matcher.find()) {
+            count++;
+        }
+        return count;
+    }
 }
